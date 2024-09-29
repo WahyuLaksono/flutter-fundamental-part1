7feba1e0a3b6316709fec0ac29ce507ede7a5c44
Wayyy <wahyugfc81@gmail.com>
2024-09-29 15:05:07 +0700

    tambah gitignore

---
+++ lib/basic_widget/text_widget2.dart
@@ -0,0 +1,9 @@
+import "package:flutter/material.dart";
+import "package:flutter/cupertino.dart";
+
+class MyTextWidget2 extends StatelessWidget {
+  const MyTextWidget2({Key ? key}) : super (key: key);
+  Widget build(BuildContext context) {
+    return const Text("Aku disini");
+  }
+}
